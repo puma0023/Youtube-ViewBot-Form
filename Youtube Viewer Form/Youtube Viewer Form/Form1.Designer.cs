8aff80fbd16480e4e529c541232a23507aaee9ea
Lolys <79699460+Lolyss1337@users.noreply.github.com>
2022-01-25 10:30:57 +0900

    Add files via upload

---
+++ Youtube Viewer Form/Youtube Viewer Form/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Youtube_Viewer_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Load Proxy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Video Id";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(359, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(30, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 38);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CBT VIEWER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(46, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bot :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(92, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(101, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(46, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Error :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(124, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(46, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 21);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Viewers :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(94, 168);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "None";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(46, 167);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 21);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Title:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(312, 138);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 21);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Proxies :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(382, 139);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 20);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 312);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Youtube Viewer Form/Youtube Viewer Form/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using Leaf.xNet;
+using Youtube_Viewers.Helpers;
+using System.IO;
+
+namespace Youtube_Viewer_Form
+{
+    public partial class Form1 : Form
+    {
+        static string id = "";
+        static int threadsCount = 1000;
+
+        static ProxyQueue scraper;
+        static ProxyType proxyType = ProxyType.HTTP;
+
+        static int botted = 0;
+        static int errors = 0;
+
+        static string viewers = "Parsing...";
+        static string title = "Parsing...";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string buildUrl(Dictionary<string, string> args)
+        {
+            var url = "https://s.youtube.com/api/stats/watchtime?";
+            foreach (var arg in args)
+            {
+                url += $"{arg.Key}={arg.Value}&";
+            }
+            return url;
+        }
+
+        private void Log()
+        {
+            while (true)
+            {
+                label4.Text = botted.ToString();
+                label5.Text = errors.ToString();
+                label7.Text = viewers.ToString();
+                label9.Text = title;
+                Thread.Sleep(250);
+            }
+        }
+
+
+        private void Worker()
+        {
+            while (true)
+            {
+                try
+                {
+                    using (var req = new HttpRequest()
+                    {
+                        Proxy = scraper.Next(),
+                        Cookies = new CookieStorage()
+                    })
+                    {
+                        req.UserAgentRandomize();
+                        req.Cookies.Container.Add(new Uri("https://www.youtube.com"), new Cookie("CONSENT", "YES+cb.20210629-13-p0.en+FX+407"));
+
+                        var sres = req.Get($"https://www.youtube.com/watch?v={textBox1.Text}").ToString();
+                        var viewersTemp = string.Join("", RegularExpressions.Viewers.Match(sres).Groups[1].Value.Where(char.IsDigit));
+
+                        if (!string.IsNullOrEmpty(viewersTemp))
+                            viewers = viewersTemp;
+
+                        title = RegularExpressions.Title.Match(sres).Groups[1].Value;
+
+                        var url = RegularExpressions.ViewUrl.Match(sres).Groups[1].Value;
+                        url = url.Replace(@"\u0026", "&").Replace("%2C", ",").Replace(@"\/", "/");
+
+                        var query = System.Web.HttpUtility.ParseQueryString(url);
+
+                        var cl = query.Get(query.AllKeys[0]);
+                        var ei = query.Get("ei");
+                        var of = query.Get("of");
+                        var vm = query.Get("vm");
+                        var cpn = GetCPN();
+
+                        var start = DateTime.UtcNow;
+
+                        var st = random.Next(1000, 10000);
+                        var et = GetCmt(start);
+                        var lio = GetLio(start);
+
+                        var rt = random.Next(10, 200);
+
+                        var lact = random.Next(1000, 8000);
+                        var rtn = rt + 300;
+
+                        var args = new Dictionary<string, string>
+                        {
+                            ["ns"] = "yt",
+                            ["el"] = "detailpage",
+                            ["cpn"] = cpn,
+                            ["docid"] = textBox1.Text,
+                            ["ver"] = "2",
+                            ["cmt"] = et.ToString(),
+                            ["ei"] = ei,
+                            ["fmt"] = "243",
+                            ["fs"] = "0",
+                            ["rt"] = rt.ToString(),
+                            ["of"] = of,
+                            ["euri"] = "",
+                            ["lact"] = lact.ToString(),
+                            ["live"] = "dvr",
+                            ["cl"] = cl,
+                            ["state"] = "playing",
+                            ["vm"] = vm,
+                            ["volume"] = "100",
+                            ["cbr"] = "Firefox",
+                            ["cbrver"] = "83.0",
+                            ["c"] = "WEB",
+                            ["cplayer"] = "UNIPLAYER",
+                            ["cver"] = "2.20201210.01.00",
+                            ["cos"] = "Windows",
+                            ["cosver"] = "10.0",
+                            ["cplatform"] = "DESKTOP",
+                            ["delay"] = "5",
+                            ["hl"] = "en_US",
+                            ["rtn"] = rtn.ToString(),
+                            ["aftm"] = "140",
+                            ["rti"] = rt.ToString(),
+                            ["muted"] = "0",
+                            ["st"] = st.ToString(),
+                            ["et"] = et.ToString(),
+                            ["lio"] = lio.ToString()
+                        };
+
+                        string urlToGet = buildUrl(args);
+                        req.AcceptEncoding = "gzip, deflate";
+                        req.AddHeader("Host", "www.youtube.com");
+                        req.Get(urlToGet.Replace("watchtime", "playback"));
+
+                        req.AcceptEncoding = "gzip, deflate";
+                        req.AddHeader("Host", "www.youtube.com");
+                        req.Get(urlToGet);
+
+                        Interlocked.Increment(ref botted);
+
+                    }
+                }
+                catch
+                {
+                    Interlocked.Increment(ref errors);
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        public static double GetCmt(DateTime date)
+        {
+            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var start = date.ToUniversalTime() - origin;
+            var now = DateTime.UtcNow.ToUniversalTime() - origin;
+            var value = (now.TotalSeconds - start.TotalSeconds).ToString("#.000");
+            return double.Parse(value);
+        }
+
+        public static double GetLio(DateTime date)
+        {
+            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var start = date.ToUniversalTime() - origin;
+            var value = start.TotalSeconds.ToString("#.000");
+            return double.Parse(value);
+        }
+
+        private static Random random = new Random();
+        public static string GetCPN()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+            return new string(Enumerable.Repeat(chars, 16)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 1500; i++)
+            {
+                List<Thread> threads = new List<Thread>();
+
+                Thread t = new Thread(Worker);
+                t.Start();
+                threads.Add(t);
+            }
+
+            List<Thread> Logthread = new List<Thread>();
+
+            Thread logWorker = new Thread(Log);
+            logWorker.Start();
+            Logthread.Add(logWorker);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Proxy list (*.txt)|*.txt";
+
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            scraper = new ProxyQueue(File.ReadAllText(dialog.FileName), proxyType);
+
+            label12.Text = scraper.Length.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
